51d9dde99aa3d8543bc0158a21456e36a8e4ac34
jose julian Lopez Huacuja <jose.lopez4636@alumnos.udg.mx>
2025-10-15 03:09:57 -0600

    Initial Commit

---
+++ Vision-Tests/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vision-Tests
+//
+//  Created by Jose julian Lopez on 15/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vision-Tests/Vision_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_TestsApp.swift
+//  Vision-Tests
+//
+//  Created by Jose julian Lopez on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Vision_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
